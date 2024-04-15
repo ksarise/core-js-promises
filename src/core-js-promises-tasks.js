b1aeef1b5a49127fe5456a4e87541ba7fda14dd1
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2024-04-16 04:54:35 +0500

    feat: finish queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -136,8 +136,14 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return promises.reduce((previousPromise, nextPromise) => {
+    return previousPromise.then((result) => {
+      return nextPromise.then((nextResult) => {
+        return result + nextResult;
+      });
+    });
+  }, Promise.resolve(''));
 }
 
 module.exports = {
